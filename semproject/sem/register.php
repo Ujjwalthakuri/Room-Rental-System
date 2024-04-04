9c60e3587cbb52dc5cecb7bb8e2d04b92e617d29
Ujjwal Thakuri <141559383+Ujjwalthakuri@users.noreply.github.com>
2024-04-04 15:45:02 +0545

    Update register.php

--- semproject/sem/register.php
+++ semproject/sem/register.php
@@ -9,6 +9,10 @@
    $roll=$_POST['roll'];
    $password=$_POST['password'];
 
+   $checkEmail="select * from crud where email='$email' "; 
+   $checkEmail_check = mysqli_query($con, $checkEmail); 
+   if(mysqli_num_rows($checkEmail_check)>0){ echo "Email already exist"; } 
+   else{
    $sql="insert into crud(fname, lname, address, number,  email, roll, password) values ('$fname', '$lname', '$address', '$number', '$email',  $roll, '$password')";
    // echo "$fname";
    $result=mysqli_query($con, $sql);
@@ -19,6 +23,6 @@
       die (nysqli_error($con));
    }
  }
-
+ }
 
 ?>
